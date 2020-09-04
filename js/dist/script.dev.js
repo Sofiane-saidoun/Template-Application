c8ed60dc7472289a1ba17191090222d17a68bc19
Sofiane <sofiane.saidoun@gmail.com>
2020-09-04 20:04:59 +0200

    *

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,37 +1,38 @@
+"use strict";
+
 $(document).ready(function () {
   /* navigation bar color change */
-  var changeColor = function () {
+  var changeColor = function changeColor() {
     /*console.log("scroll");*/
     var scroll_start = 0;
     var startchange = $("#start");
     var offset = startchange.offset();
+
     if (startchange.length) {
       $(document).scroll(function () {
         scroll_start = $(this).scrollTop();
+
         if (scroll_start > offset.top) {
           $(".navbar").addClass("bg-cloud");
         } else {
           $(".navbar").removeClass("bg-light");
           $(".navbar").removeClass("bg-cloud");
-
           /*console.log("portfolio out");*/
         }
       });
     }
   };
-  var scroll = function () {
+
+  var scroll = function scroll() {
     $(".page-scroll a").click(function (event) {
       event.preventDefault();
       var $anchor = $(this);
-
-      $("html, body").animate(
-        {
-          scrollTop: $($anchor.attr("href")).offset().top,
-        },
-        1000
-      );
+      $("html, body").animate({
+        scrollTop: $($anchor.attr("href")).offset().top
+      }, 1000);
     });
   };
+
   scroll();
   changeColor();
 });
